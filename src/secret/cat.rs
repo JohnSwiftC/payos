db7fcec459deecbea2cfb164840ee4737c94e9d9
JohnSwiftC <johnswift@live.com>
2026-03-31 22:26:24 -0400

    Hell on earth: Center an image attempt 4

--- src/secret/cat.rs
+++ src/secret/cat.rs
@@ -17,16 +17,36 @@ use crossterm::event::{Event, KeyCode, KeyEventKind};
 use image::imageops::FilterType;
 
 pub fn render(app: &App, area: Rect, buf: &mut Buffer) {
-    let resized =
-        app.cat_image
-            .resize_exact(area.width.into(), area.height.into(), FilterType::Triangle);
+    let img_w = app.cat_image.width() as u32;
+    let img_h = app.cat_image.height() as u32;
+
+    // we are using full height
+    let render_height = area.height as u32;
+
+    // preserve aspect ratio
+    let render_width = ((render_height * img_w) / img_h) as u16;
+
+    // clamp so it doesn't exceed area
+    let render_width = render_width.min(area.width);
+    let x = area.x + (area.width - render_width) / 2;
+
+    let centered = Rect {
+        x,
+        y: area.y,
+        width: render_width,
+        height: area.height,
+    };
     let protocol = app
         .picker
-        .new_protocol(resized, area, ratatui_image::Resize::Fit(None))
+        .new_protocol(
+            app.cat_image.clone(),
+            centered,
+            ratatui_image::Resize::Scale(Some(ratatui_image::FilterType::Triangle)),
+        )
         .unwrap();
 
     let cat_widget = Image::new(&protocol);
-    cat_widget.render(area, buf);
+    cat_widget.render(centered, buf);
 }
 
 pub fn event_callback(app: &mut App, event: Event) -> Option<PageSignal> {
