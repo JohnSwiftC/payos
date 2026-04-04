fa1ee265c89ae0c62cb815f247dce2a9f63d2c66
JohnSwiftC <johnswift@live.com>
2026-04-04 17:31:54 -0400

    Clean warnings

--- src/popup/unauth.rs
+++ src/popup/unauth.rs
@@ -1,6 +1,5 @@
 use crate::App;
-use crate::{Interupt, PageSignal};
-use crossterm::event::{Event, KeyCode, KeyEventKind};
+use crate::Interupt;
 use ratatui::style::Stylize;
 use ratatui::text::{Line, Text};
 use ratatui::widgets::Widget;
@@ -9,7 +8,7 @@ use ratatui::{
     layout::{Constraint, Rect},
 };
 
-pub fn render(app: &mut App, area: Rect, buf: &mut Buffer) {
+pub fn render(_: &mut App, area: Rect, buf: &mut Buffer) {
     let text = Text::from(Line::from("Unauthorized".red()));
     let centered = area.centered(
         Constraint::Length(text.width() as u16),
@@ -19,7 +18,7 @@ pub fn render(app: &mut App, area: Rect, buf: &mut Buffer) {
     text.render(centered, buf);
 }
 
-pub fn callback(app: &mut App) {
+pub fn callback(_: &mut App) {
     std::thread::sleep(std::time::Duration::from_secs(3));
 }
 
