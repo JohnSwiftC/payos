9ced5b3d29e9081bbc2d7156035c653554546883
John Swift <johnswift@live.com>
2026-05-19 05:08:33 -0400

    Rip out wheel for later

--- src/funmenu/wheel.rs
+++ src/funmenu/wheel.rs
@@ -12,25 +12,9 @@ pub struct Wheel {
 }
 
 impl Interupt for Wheel {
-    fn render(&mut self, _: &mut App, area: Rect, buf: &mut Buffer) {
-        if self.curr != "Test 1" {
-            self.curr = "Test 1";
-        } else {
-            self.curr = "Test 2";
-        }
+    fn render(&mut self, _: &mut App, area: Rect, buf: &mut Buffer) {}
 
-        let text = Text::from(self.curr);
-
-        text.render(area, buf);
-    }
-
-    fn callback(&mut self, app: &mut App) {
-        std::thread::sleep(Duration::from_secs(3));
-
-        if self.curr == "Test 1" {
-            app.interupt = Some(interupt());
-        }
-    }
+    fn callback(&mut self, app: &mut App) {}
 }
 
 pub fn interupt() -> Box<Wheel> {
